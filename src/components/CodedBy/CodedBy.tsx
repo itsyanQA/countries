8e108ea6617c2d99344badce810b497f05c7651a
stron <yaneliyahu83@gmail.com>
2023-09-24 02:11:11 +0300

    Add theme to CodedBy.tsx text and add missing space

--- src/components/CodedBy/CodedBy.tsx
+++ src/components/CodedBy/CodedBy.tsx
@@ -1,10 +1,12 @@
-import React from "react";
 import "./CodedBy.scss";
+import useThemeContext from "../../hooks/use-theme-context";
 
 export default function CodedBy() {
+  const { themeClass } = useThemeContext();
+
   return (
-    <p className="coded-by">
-      Challenge by{" "}
+    <p className={`coded-by ${themeClass}`}>
+      Challenge by&nbsp;
       <a href="https://www.frontendmentor.io/challenges/rest-countries-api-with-color-theme-switcher-5cacc469fec04111f7b848ca">
         Frontend Mentor
       </a>
