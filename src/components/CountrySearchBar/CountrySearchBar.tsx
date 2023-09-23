0c4ae8180220b13a86190418719dd0ca457cd18f
stron <yaneliyahu83@gmail.com>
2023-09-23 18:57:27 +0300

    Create CountrySearchBar.tsx component

---
+++ src/components/CountrySearchBar/CountrySearchBar.tsx
@@ -0,0 +1,47 @@
+import { Input as SearchBar } from "../../styled/Input";
+import SearchIcon from "@mui/icons-material/Search";
+import { Dispatch, SetStateAction } from "react";
+import useThemeContext from "../../hooks/use-theme-context";
+
+type CountrySearchBarProps = {
+  searchValue: string;
+  setSearchValue: Dispatch<SetStateAction<string>>;
+};
+
+export default function CountrySearchBar({ searchValue, setSearchValue }: CountrySearchBarProps) {
+  const { theme } = useThemeContext();
+  const textColor = theme === "light" ? "var(--dark-blue)" : "var(--white)";
+  const searchIconStyle = {
+    light: {
+      opacity: "50%",
+    },
+    dark: {
+      color: "var(--white)",
+    },
+  };
+
+  return (
+    <SearchBar
+      value={searchValue}
+      onChange={(e) => setSearchValue(e.target.value)}
+      placeholder="Search for a country..."
+      InputProps={{
+        startAdornment: <SearchIcon sx={{ padding: "0 20px 0 10px", ...searchIconStyle[theme] }} />,
+      }}
+      sx={{
+        backgroundColor: theme === "dark" ? "var(--dark-blue)" : "var(--white)",
+        color: textColor,
+        "& .MuiInputBase-root": {
+          color: textColor,
+        },
+      }}
+      inputProps={{
+        sx: {
+          "&::placeholder": {
+            color: textColor,
+          },
+        },
+      }}
+    />
+  );
+}
