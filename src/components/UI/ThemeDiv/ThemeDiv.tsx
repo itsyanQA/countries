07aa1f37cf66f95356c71089f1c947788fbb5b9e
stron <yaneliyahu83@gmail.com>
2023-09-23 22:46:45 +0300

    Add scroll to top useEffect and remove redundant prop from ThemeDiv.tsx

--- src/components/UI/ThemeDiv/ThemeDiv.tsx
+++ src/components/UI/ThemeDiv/ThemeDiv.tsx
@@ -1,19 +1,22 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect } from "react";
 import useThemeContext from "../../../hooks/use-theme-context";
 import "./ThemeDiv.scss";
 
 type ThemeDivProps = {
   children: ReactNode;
-  props?: any;
 };
 
-export default function ThemeDiv({ children, props }: ThemeDivProps) {
+export default function ThemeDiv({ children }: ThemeDivProps) {
   const { theme } = useThemeContext();
+
+  useEffect(() => window.scroll(0, 0), []);
+
   return (
     <main
-      {...props}
       className="theme-container"
-      style={{ backgroundColor: theme === "light" ? "var(--very-light-gray)" : "var(--very-dark-blue-1)" }}
+      style={{
+        backgroundColor: theme === "light" ? "var(--very-light-gray)" : "var(--very-dark-blue-1)",
+      }}
     >
       {children}
     </main>
